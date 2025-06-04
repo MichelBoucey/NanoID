2bbc277a2ea273376b2f9b4af6a2dde4e7a5162d
Michel Boucey <michel.boucey@gmail.com>
2025-06-04 12:10:56 +0200

    Nix derivation updated

--- nanoid.nix
+++ nanoid.nix
@@ -1,9 +1,9 @@
 { mkDerivation, aeson, base, bytestring, cereal, extra, lib
-, mwc-random, optparse-applicative, text
+, mwc-random, optparse-applicative, text, bytestring-encodings
 }:
 mkDerivation {
   pname = "NanoID";
-  version = "3.4.0.2";
+  version = "3.4.1.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -11,7 +11,7 @@ mkDerivation {
     aeson base bytestring cereal extra mwc-random text
   ];
   executableHaskellDepends = [
-    base bytestring mwc-random optparse-applicative
+    base bytestring mwc-random optparse-applicative bytestring-encodings
   ];
   description = "NanoID generator";
   license = lib.licenses.bsd3;
